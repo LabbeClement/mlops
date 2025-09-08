bc351b7d2dfde318891dc3c95a6118a2da4a6809
Clément LABBE <clement.labbe@epita.fr>
2025-09-08 15:08:56 +0200

    lvl 0

--- webapp.py
+++ webapp.py
@@ -1,5 +1,5 @@
 #Level 0 (on your local machine) 
-from turtle import st
+from streamlit import st
 from fastapi import FastAPI
 import joblib
 
@@ -13,10 +13,5 @@ def predict(size: float = 0, bedrooms: int = 0, garden: int = 0):
 
 #Level 0.5 (on a remote machine)
 
-# 1. Connect via SSH to the provided machine (ubuntu@20.54.250.141) password: Supermotdepasse!42.
-
-
-# 2. Create a folder with your initial (td for Tom dupont for isntance)
-# 3. Using scp or git clone, deploy your code on a remote machine (provided) in your folder
 # 4. Launch your web service on the remote machine
 # 5. Verify that you and a colleague can access your web service and get predictions
